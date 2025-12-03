04414e0b3ea9108c89c89a55e5b8b80030e85fa8
corvinkoegler <corvin.koegler.hpi@gmail.com>
2025-12-03 12:31:03 +0100

    added basic thread communication objects

--- gcode_interface.py
+++ gcode_interface.py
@@ -1,4 +1,46 @@
+import threading
+import datetime
+from threading import Event
+from concurrent.futures import ThreadPoolExecutor
+
+
+class Message:
+    _timestamp: datetime.Datetime
+    _text: str
+    _raw: any
+
+    def __init__(self, arg: str | bytes):
+        self.timestamp = datetime.datetime.now()
+
+        if type(arg) == bytes:
+            self.raw = arg
+            self.text = arg.decode()
+            return
+
+        try:
+            self.text = str(arg)
+            self.raw = arg
+        except ValueError:
+            raise NotImplementedError(f"Received {type(arg)}")
+
+    @property
+    def text(self):
+        return self._text
+
+    @property
+    def raw(self):
+        return self._raw
+
+    @property
+    def timestamp(self):
+        return self._timestamp
+
+
 class GCodeInterface:
+    """send and receive messages through websockets without blocking the main thread"""
+
+    _exit_flag: Event
+    _thread_executor: ThreadPoolExecutor
 
     def __init__(self):
         pass
@@ -11,6 +53,22 @@ class GCodeInterface:
         """used for context manager syntax"""
         pass
 
+    def _recv_thread(self, exit_flag: Event):
+        """run a separate thread for receiving messages"""
+
+        thread_name = threading.current_thread().name
+
+        while not exit_flag.is_set():
+            pass
+
+    def _send_thread(self, exit_flag: Event):
+        """run a separate thread for sending messages"""
+
+        thread_name = threading.current_thread().name
+
+        while not exit_flag.is_set():
+            pass
+
     def open(self):
         """fallback for processes without context managers"""
         self.__enter__()
@@ -20,9 +78,9 @@ class GCodeInterface:
         self.__exit__()
 
     def send(self):
-        """send any message through a searate thread"""
+        """send any message text through a searate thread"""
         pass
 
     def recv(self):
-        """receive any message from a seperate thread"""
+        """receive any message text from a seperate thread"""
         pass
