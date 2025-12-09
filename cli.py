6840849c8ef837a5c5a8f9391f479b3fcde2f09d
corvinkoegler <corvin.koegler.hpi@gmail.com>
2025-12-09 16:56:17 +0100

    non displaying cli can now be quit with ctrl-c

--- cli.py
+++ cli.py
@@ -1,4 +1,16 @@
 from prompt_toolkit.application import Application
+from prompt_toolkit.key_binding import KeyBindings
+from prompt_toolkit.key_binding.bindings.focus import focus_next, focus_previous
+from prompt_toolkit.application.current import get_app
+
+key_bindings = KeyBindings()
+
+@key_bindings.add("c-c")
+def exit(event) -> None:
+    get_app().exit()
+
+key_bindings.add("tab")(focus_next)
+key_bindings.add("s-tab")(focus_previous)
 
 application = Application(full_screen=True, mouse_support=True)
 
