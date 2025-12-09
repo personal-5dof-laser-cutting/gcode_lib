8909457d332a7634897b9ebd641e7cac444e1ca9
corvinkoegler <corvin.koegler.hpi@gmail.com>
2025-12-09 19:06:44 +0100

    submitting clears prompt line

--- cli.py
+++ cli.py
@@ -4,10 +4,25 @@ from prompt_toolkit.key_binding.bindings.focus import focus_next, focus_previous
 from prompt_toolkit.application.current import get_app
 from prompt_toolkit.layout import Layout, Dimension, HSplit, ScrollablePane
 from prompt_toolkit.widgets import Frame, TextArea, Label
+from prompt_toolkit.buffer import Buffer
+
+# --- DATA STRUCTURES --- #
+
+gcode_labels = [
+    Label(text="Gcode Line 1"),
+    Label(text="Gcode Line 2"),
+    Label(text="... many more lines ..."),
+    Label(text="Gcode Line N"),
+]
+
+gcode_container = HSplit(
+    gcode_labels
+)
 
 # --- KEY BINDINGS --- #
 
 key_bindings = KeyBindings()
+
 
 @key_bindings.add("c-q")
 @key_bindings.add("c-c")
@@ -18,31 +33,31 @@ def exit(event) -> None:
 key_bindings.add("tab")(focus_next)
 key_bindings.add("s-tab")(focus_previous)
 
-@key_bindings.add("c-q")
-@key_bindings.add("c-c")
-def exit(event) -> None:
-    get_app().exit()
-
 # -- COMMAND PROMPT -- #
 
-input_bindings = KeyBindings()
-@input_bindings.add('enter')
-def handle_submit(event):
-    event.cli.current_buffer.text = ""
+def handle_input_submit(buffer: Buffer):
+    command_text = buffer.text
+    buffer.text = ""
+
+    gcode_labels.append(Label(text="Hello World!"))
+    
+    get_app().invalidate()
 
 
 # --- LAYOUT --- #
 
 scrollable_content = HSplit(
-    [
-        Label(text="Gcode Line 1"),
-        Label(text="Gcode Line 2"),
-        Label(text="... many more lines ..."),
-        Label(text="Gcode Line N"),
-    ]
+    gcode_labels
 )
 
-input_prompt = TextArea(text=f"label", width=Dimension(), multiline=False, prompt="> ",)
+input_prompt = TextArea(
+    text=f"label",
+    width=Dimension(),
+    multiline=False,
+    prompt="> ",
+    accept_handler=handle_input_submit,
+)
+
 root_container = HSplit(
     [
         Frame(ScrollablePane(scrollable_content)),
