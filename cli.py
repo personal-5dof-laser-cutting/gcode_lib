e87ae980567648e6e4ce95a173f1cec873fee78f
corvinkoegler <corvin.koegler.hpi@gmail.com>
2025-12-09 16:24:55 +0100

    added cli base

--- cli.py
+++ cli.py
@@ -1,5 +1,11 @@
+from prompt_toolkit.application import Application
+
+application = Application(full_screen=True, mouse_support=True)
+
+
 def main():
-    pass
+    application.run()
+
 
 if __name__ == "__main__":
     main()
