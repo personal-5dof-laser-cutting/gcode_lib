d35e31cb552525072740c53527de138774e42cd1
corvinkoegler <corvin.koegler.hpi@gmail.com>
2026-08-12 14:53:49 +0200

    added basic example

---
+++ examples/basic-serial.py
@@ -0,0 +1,24 @@
+from gcode_lib import CommunicationProxy, FluidNCSerialDriver
+import time
+import logging
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s - %(levelname)s - %(message)s"
+)
+
+log = logging.getLogger(__name__)
+
+if __name__ == "__main__":
+    driver = FluidNCSerialDriver("/dev/ttyUSB0")
+    proxy = CommunicationProxy(driver=driver)
+
+    log.info("Connecting")
+    proxy.connect(setup_reporting=False)
+    log.info("Sending Message")
+    proxy.send_message("?")
+    time.sleep(5)
+    log.info("Reading Message")
+    print(proxy.read_message())
+    log.info("Closing")
+    proxy.close()
