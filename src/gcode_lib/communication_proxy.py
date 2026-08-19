c5e441177b612ef0fe1ff49ddc5ae2ea3cedf557
Ediiiiiiiiiiiiiiiiiiiiiiii <edgar.bennemann@student.hpi.uni-potsdam.de>
2026-08-19 11:39:16 +0200

    hotfix: provide interface for realtime commands

--- src/gcode_lib/communication_proxy.py
+++ src/gcode_lib/communication_proxy.py
@@ -275,6 +275,11 @@ class CommunicationProxy:
         log.debug("Sending inferred message to worker: %r", message)
         self._worker.cmd_queue.put(("SEND", message))
 
+    def send_realtime(self, message: str) -> None:
+        self._verify_connection()
+        log.debug("Sending inferred realtime message to worker: %r", message)
+        self._worker.cmd_queue.put(("SEND_REALTIME", message))
+
     def read_message(self, timeout: Optional[float] = None) -> Optional[str]:
         """
         Fetch the next incoming message from the worker response queue.
